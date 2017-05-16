4748f4cbd252c2593f1d90624541ace37b18569c
范博 <fanbo@fanbodeMacBook-Pro.local>
2017-05-16 22:25:08 +0800

    first commit

---
+++ HaloKitGlobal/BGMonitorVc.h
@@ -0,0 +1,13 @@
+//
+//  BGMonitorVc.h
+//  HaloKitGlobal
+//
+//  Created by 范博 on 2017/5/16.
+//  Copyright © 2017年 范博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGMonitorVc : UIViewController
+
+@end
---
+++ HaloKitGlobal/BGMonitorVc.m
@@ -0,0 +1,37 @@
+//
+//  BGMonitorVc.m
+//  HaloKitGlobal
+//
+//  Created by 范博 on 2017/5/16.
+//  Copyright © 2017年 范博. All rights reserved.
+//
+
+#import "BGMonitorVc.h"
+
+@interface BGMonitorVc ()
+
+@end
+
+@implementation BGMonitorVc
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
